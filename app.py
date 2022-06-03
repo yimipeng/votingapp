4e44a788e5423a8292e1238e3a3572b2877fbbf5
Yiming Peng <91288087+yimipeng@users.noreply.github.com>
2022-06-02 22:05:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,40 @@ import boto3
 from multiprocessing import Pool
 from multiprocessing import cpu_count
 
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
+
+from opentelemetry import propagate
+from opentelemetry.sdk.extension.aws.trace.propagation.aws_xray_format import AwsXRayFormat
+
+from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
+from opentelemetry.sdk.extension.aws.trace.propagation.aws_xray_format import (
+    TRACE_ID_DELIMITER,
+    TRACE_ID_FIRST_PART_LENGTH,
+    TRACE_ID_VERSION,
+)
+
+
+otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317")
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.set_tracer_provider(TracerProvider(active_span_processor=span_processor, id_generator=AwsXRayIdGenerator()))
+
+propagate.set_global_textmap(AwsXRayFormat())
+
 app = Flask(__name__)
+
+BotocoreInstrumentor().instrument()
+# Initialize `Instrumentor` for the `requests` library
+RequestsInstrumentor().instrument()
+# Initialize `Instrumentor` for the `flask` web framework
+FlaskInstrumentor().instrument_app(app)
 
 cors = CORS(app, resources={r"/api/*": {"Access-Control-Allow-Origin": "*"}})
 
